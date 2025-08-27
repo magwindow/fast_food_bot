451177f5ad5e3fe7f5b0aa81de11e7999e71ae74
magwindow <antosha.petrakov@yandex.ru>
2025-08-27 17:21:01 +0300

    Added function get contact users

--- database/db_utils.py
+++ database/db_utils.py
@@ -1,7 +1,46 @@
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
+from sqlalchemy import update, select
 
 from database.engine import engine
-
+from database.models import Users, Carts
 
 with Session(engine) as session:
     db_session = session
+
+
+def db_register_user(full_name: str, chat_id: int) -> bool:
+    """Первая регистрация пользователя с доступными данными"""
+    try:
+        # Если пользователь не существует
+        query = Users(name=full_name, telegram=chat_id)
+        db_session.add(query)
+        db_session.commit()
+        return False
+    except IntegrityError:
+        # Если пользователь существует
+        db_session.rollback()
+        return True
+
+
+def db_update_user(chat_id: int, phone: str):
+    """Дополняем данные пользователя телефонным номером"""
+    query = update(Users).where(Users.telegram == chat_id).values(phone=phone)
+    db_session.execute(query)
+    db_session.commit()
+
+
+def db_create_user_cart(chat_id: int):
+    """Создаем временную корзину для пользователя"""
+    try:
+        subquery = db_session.scalar(select(Users).where(Users.telegram == chat_id))
+        query = Carts(user_id=subquery.id)
+        db_session.add(query)
+        db_session.commit()
+        return True
+    except IntegrityError:
+        # Если пользователь уже имеет корзину
+        db_session.rollback()
+    except AttributeError:
+        # Если отправил контакт анонимный пользователь
+        db_session.rollback()
--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import os
 import asyncio
 from dotenv import load_dotenv, find_dotenv
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 
 load_dotenv(find_dotenv())
+
+from database.db_utils import db_register_user, db_update_user, db_create_user_cart
+from keyboards.reply_kb import share_phone_button
 
 bot = Bot(token=os.getenv('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
@@ -17,6 +20,29 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def command_start(message: Message):
     await message.answer(f'Здравствуйте, <b>{message.from_user.full_name}</b>!\n Вас приветствует бот доставки шаурмы!')
+    await start_register_user(message)
+
+
+@dp.message(F.contact)
+async def update_user_info_finish_register(message: Message):
+    """Обновление данных пользователя его контактом"""
+    chat_id = message.chat.id
+    phone = message.contact.phone_number
+    db_update_user(chat_id, phone)
+    if db_create_user_cart(chat_id):
+        await message.answer('Вы успешно зарегистрированы!')
+    # await show_main_menu(message)
+
+
+async def start_register_user(message: Message):
+    """Первая регистрация пользователя с проверкой на существование"""
+    full_name = message.from_user.full_name
+    chat_id = message.chat.id
+    if db_register_user(full_name=full_name, chat_id=chat_id):
+        await message.answer(f'Успешная авторизация, <b>{full_name}</b>!')
+    else:
+        await message.answer('Для связи с вами, нам нужен ваш контактный номер.',
+                             reply_markup=share_phone_button())
 
 
 async def main():
